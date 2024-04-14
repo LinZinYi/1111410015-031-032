b80301d0a5565b0a2d1d79e90b33b04ee167b906
林姿吟 <134450132+LinZinYi@users.noreply.github.com>
2024-04-15 00:42:45 +0800

    031程式碼

---
+++ 1111410015_林姿吟031.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace _1111410015_林姿吟031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList words = new ArrayList();
+            ArrayList Ans=new ArrayList();
+            string str;
+            int count = 0;
+            Console.Write("words=");
+            str = Console.ReadLine();
+
+            words.AddRange(str.Split(' ').ToArray());
+            char x;
+            Console.Write("x=");
+            x= Convert.ToChar(Console.ReadLine().ToLower());
+
+            if (words.Count <= 50 && str.Length <= 50)
+            {
+                for (int i = 0; i < words.Count; i++)
+                {
+                    if (Convert.ToString(words[i]).IndexOf(x) != -1)
+                    {
+                        Ans.Add(i);
+                        count++;
+                    }
+                }
+
+                if (count == 0) 
+                {
+                    Console.WriteLine("[]");
+                }
+                else
+                {
+                    Console.Write("[");
+                    foreach (int s in Ans)
+                    {
+                        Console.Write(s + " ");
+                    }
+                    Console.WriteLine("]");
+                }
+            }
+
+
+
+        }
+    }
+}
